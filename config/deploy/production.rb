bfd46cfebc14968350846db874f2cbe453d5d5ae
Cate Uselton <cathode.use@gmail.com>
2015-01-27 13:42:27 -0800

    fixing production username for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@
 
 server '54.200.109.118',
  user: 'ubuntu',
-  roles: %w{web app},
+  roles: %w{web app db},
   ssh_options: {
     keys: %w(/Users/catusel/.ssh/ec2-ada.pem),
     forward_agent: false,
