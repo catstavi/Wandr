c0abb7047dd0148121072fcdc1bebc5f85e690b4
Cate Uselton <cathode.use@gmail.com>
2015-02-03 15:01:28 -0800

    schema conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150202192317) do
-=======
 ActiveRecord::Schema.define(version: 20150203175612) do
->>>>>>> cate-instagram
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
