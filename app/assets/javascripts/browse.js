742973987dce21e5e0aec283b93ad144f9a1641d
Cate Uselton <cathode.use@gmail.com>
2015-02-03 16:12:41 -0800

    click through photos so AWESOME

--- app/assets/javascripts/browse.js
+++ app/assets/javascripts/browse.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-  console.log("lets go!")
-  var active_div = $('#all').children().first();
+  var n = 1
+  var active_div = $('#all').children().eq(n);
+  switchPhoto(active_div, n);
+});
+
+function switchPhoto(active_div, n) {
+  n +=1
   var photo = document.createElement("img");
   photo.setAttribute("src", active_div.attr('id'));
-  active_div.appendChild(photo);
-})
+  active_div.append(photo);
+  active_div.click(function() {
+    active_div.children().remove()
+    switchPhoto($('#all').children().eq(n), n)
+  })
+};
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,9 @@ class LocationsController < ApplicationController
   def browse
     # @data = YelpRequester.request(session[:user_lat], session[:user_long])
     # Location.record_from_yelp(@data)
-    @photo_hash= InstagramRequester.photos_by_user_location(session[:user_lat], session[:user_long])
+    @lat = session[:user_lat]
+    @long = session[:user_long]
+    @photo_hash= InstagramRequester.photos_by_user_location(@lat, @long)
 
     # scroll through photos loaded by db/api, javascript interactive
   end
