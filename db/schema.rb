2c3e581fd5be29eac0288c4218a0be7fae2ee4d2
Cate Uselton <cathode.use@gmail.com>
2015-02-05 14:21:05 -0800

    added photo model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203175612) do
+ActiveRecord::Schema.define(version: 20150205222053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20150203175612) do
     t.datetime "updated_at"
     t.string   "place_id"
     t.string   "city"
+  end
+
+  create_table "photos", force: true do |t|
+    t.string   "url"
+    t.integer  "location_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "windows", force: true do |t|
