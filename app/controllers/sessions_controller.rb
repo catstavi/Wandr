44e5cfcc6768cdb03df0a5649ad4141ccdc21be1
Cate Uselton <cathode.use@gmail.com>
2015-02-15 15:30:25 -0800

    sessions controller method prepared to save by params address string

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
 
-  def set_location
+  def geolocation
     session[:user_lat] = params[:latitude]
     session[:user_long] = params[:longitude]
+    all_ok
+  end
+
+  def all_ok
     respond_to do |format|
       format.json { render json: :pineapple, status: :ok }
     end
+  end
+
+  def address_location
+    address = params[:address]
+    a=Geokit::Geocoders::GoogleGeocoder.geocode address
+    session[:user_lat] = a.lat
+    session[:user_lng] = a.lng
+    all_ok
   end
 end
